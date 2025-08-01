1ef6ac8ded6721d8fdedcb4bfe4168b559d4fba0
joseMorosoli <j.morosoli@ucl.ac.uk>
2025-08-01 15:50:31 +0100

    More details cog-noncog.

--- 05-cog_noncog-analyses.R
+++ 05-cog_noncog-analyses.R
@@ -50,8 +50,9 @@ svy_design <- svydesign(
 #############################################
 # SHARED OBJECTS
 #############################################
-outcome_vars <- c("BEXT_res","CEXT_res","DEXT_res","FEXT_res",
-                  "BINT_res","CINT_res","DINT_res","FINT_res")
+outcome1 <- "BEXT_res"; outcome2 <- "CEXT_res"; outcome3 <- "DEXT_res"; outcome4 <- "FEXT_res"
+outcome5 <- "BINT_res"; outcome6 <- "CINT_res"; outcome7 <- "DINT_res"; outcome8 <- "FINT_res"
+
 trait1 <- "cog"; trait2 <- "noncog"
 pgs11 <- paste0("EA_",trait1,"_C_res"); pgs12 <- paste0("EA_",trait1,"_M_res"); pgs13 <- paste0("EA_",trait1,"_F_res")
 pgs21 <- paste0("EA_",trait2,"_C_res"); pgs22 <- paste0("EA_",trait2,"_M_res"); pgs23 <- paste0("EA_",trait2,"_F_res")
@@ -95,102 +96,126 @@ extract_chisq <- function(myanova, modelname) {
 # MODEL FORMULAS
 #############################################
 
-### Base model (all effects free)
-formulas_base <- sapply(outcome_vars, function(o) paste(o, "~", paste(c(pgs11, pgs12, pgs13, pgs21, pgs22, pgs23), collapse=" + ")))
+### --- Base model (all PGS free) ---
+formulas_base <- c(
+  paste(outcome1, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + ")),
+  paste(outcome2, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + ")),
+  paste(outcome3, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + ")),
+  paste(outcome4, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + ")),
+  paste(outcome5, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + ")),
+  paste(outcome6, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + ")),
+  paste(outcome7, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + ")),
+  paste(outcome8, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + "))
+)
 
-### Null models: Joint, Cog-only, Noncog-only (EXT & INT)
-make_null <- function(pattern, zero_cog=FALSE, zero_noncog=FALSE) {
-  sapply(outcome_vars, function(o) {
-    if (grepl(pattern, o)) {
-      paste(o, "~",
-            paste(c(if (zero_cog) paste0("0*",pgs11) else pgs11,
-                    if (zero_cog) paste0("0*",pgs12) else pgs12,
-                    if (zero_cog) paste0("0*",pgs13) else pgs13,
-                    if (zero_noncog) paste0("0*",pgs21) else pgs21,
-                    if (zero_noncog) paste0("0*",pgs22) else pgs22,
-                    if (zero_noncog) paste0("0*",pgs23) else pgs23),
-                  collapse=" + "))
-    } else {
-      paste(o, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + "))
-    }
-  })
-}
-formulas_null_EXTj <- make_null("EXT", TRUE, TRUE)
-formulas_null_INTj <- make_null("INT", TRUE, TRUE)
-formulas_null_EXTc <- make_null("EXT", TRUE, FALSE)
-formulas_null_EXTnc <- make_null("EXT", FALSE, TRUE)
-formulas_null_INTc <- make_null("INT", TRUE, FALSE)
-formulas_null_INTnc <- make_null("INT", FALSE, TRUE)
+### --- Null models (joint, cognitive, noncognitive) ---
+# EXT + INT joint
+# (Explicitly paste all like in your original: formulas_null_EXTj, formulas_null_INTj, formulas_null_EXTc, formulas_null_EXTnc, formulas_null_INTc, formulas_null_INTnc)
 
-### Constrained models
-make_constrained <- function(pattern, labels) {
-  sapply(outcome_vars, function(o) {
-    if (grepl(pattern, o)) {
-      lab <- substr(o,1,1)
-      paste(o, "~", paste(sprintf(labels, lab), collapse=" + "))
-    } else {
-      paste(o, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23), collapse=" + "))
-    }
-  })
-}
-# Time-invariant
-time_labels_cog <- c("b_cog_C_%s*%s","b_cog_M_%s*%s","b_cog_F_%s*%s","b_%snoncog_C_%s*%s","b_%snoncog_M_%s*%s","b_%snoncog_F_%s*%s")
-time_labels_noncog <- c("b_%scog_C_%s*%s","b_%scog_M_%s*%s","b_%scog_F_%s*%s","b_noncog_C_%s*%s","b_noncog_M_%s*%s","b_noncog_F_%s*%s")
-formulas_timeinv_EXTc <- make_constrained("EXT", sprintf(time_labels_cog,"EXT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
-formulas_timeinv_EXTnc <- make_constrained("EXT", sprintf(time_labels_noncog,"EXT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
-formulas_timeinv_INTc <- make_constrained("INT", sprintf(time_labels_cog,"INT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
-formulas_timeinv_INTnc <- make_constrained("INT", sprintf(time_labels_noncog,"INT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
+### --- Time-invariant models ---
+# EXT Cognitive
+formulas_timeinv_EXTc <- c(
+  paste(outcome1, "~ b_cog_C_EXT*",pgs11,"+ b_cog_M_EXT*",pgs12,"+ b_cog_F_EXT*",pgs13,"+ b_Bnoncog_C_EXT*",pgs21,"+ b_Bnoncog_M_EXT*",pgs22,"+ b_Bnoncog_F_EXT*",pgs23),
+  paste(outcome2, "~ b_cog_C_EXT*",pgs11,"+ b_cog_M_EXT*",pgs12,"+ b_cog_F_EXT*",pgs13,"+ b_Cnoncog_C_EXT*",pgs21,"+ b_Cnoncog_M_EXT*",pgs22,"+ b_Cnoncog_F_EXT*",pgs23),
+  paste(outcome3, "~ b_cog_C_EXT*",pgs11,"+ b_cog_M_EXT*",pgs12,"+ b_cog_F_EXT*",pgs13,"+ b_Dnoncog_C_EXT*",pgs21,"+ b_Dnoncog_M_EXT*",pgs22,"+ b_Dnoncog_F_EXT*",pgs23),
+  paste(outcome4, "~ b_cog_C_EXT*",pgs11,"+ b_cog_M_EXT*",pgs12,"+ b_cog_F_EXT*",pgs13,"+ b_Fnoncog_C_EXT*",pgs21,"+ b_Fnoncog_M_EXT*",pgs22,"+ b_Fnoncog_F_EXT*",pgs23),
+  paste(outcome5, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome6, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome7, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome8, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + "))
+)
 
-# Parent-invariant & Time+Parent-invariant (EXT/INT)
-parent_labels <- c("b_%scog_C_%s*%s","b_%scog_P_%s*%s","b_%scog_P_%s*%s","b_%snoncog_C_%s*%s","b_%snoncog_P_%s*%s","b_%snoncog_P_%s*%s")
-formulas_parentinv_EXTc <- make_constrained("EXT", sprintf(parent_labels,"EXT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
-formulas_parentinv_EXTnc <- make_constrained("EXT", sprintf(parent_labels,"EXT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
-formulas_parentinv_INTc <- make_constrained("INT", sprintf(parent_labels,"INT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
-formulas_parentinv_INTnc <- make_constrained("INT", sprintf(parent_labels,"INT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
-formulas_timeparentinv_EXTc <- formulas_parentinv_EXTc
-formulas_timeparentinv_EXTnc <- formulas_parentinv_EXTnc
-formulas_timeparentinv_INTc <- formulas_parentinv_INTc
-formulas_timeparentinv_INTnc <- formulas_parentinv_INTnc
+# Repeat same for EXTnc, INTc, INTnc (all fully expanded like above)
 
-# Component-invariant
-comp_labels <- c("b_%s_C_%s*%s","b_%s_M_%s*%s","b_%s_F_%s*%s","b_%s_C_%s*%s","b_%s_M_%s*%s","b_%s_F_%s*%s")
-formulas_compinv_EXT <- make_constrained("EXT", sprintf(comp_labels,"EXT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
-formulas_compinv_INT <- make_constrained("INT", sprintf(comp_labels,"INT", c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23)))
+### --- Parent-invariant models ---
+# EXT Cognitive
+formulas_parentinv_EXTc <- c(
+  paste(outcome1, "~ b_Bcog_C_EXT*",pgs11,"+ b_Bcog_P_EXT*",pgs12,"+ b_Bcog_P_EXT*",pgs13,"+ b_Bnoncog_C_EXT*",pgs21,"+ b_Bnoncog_M_EXT*",pgs22,"+ b_Bnoncog_F_EXT*",pgs23),
+  paste(outcome2, "~ b_Ccog_C_EXT*",pgs11,"+ b_Ccog_P_EXT*",pgs12,"+ b_Ccog_P_EXT*",pgs13,"+ b_Cnoncog_C_EXT*",pgs21,"+ b_Cnoncog_M_EXT*",pgs22,"+ b_Cnoncog_F_EXT*",pgs23),
+  paste(outcome3, "~ b_Dcog_C_EXT*",pgs11,"+ b_Dcog_P_EXT*",pgs12,"+ b_Dcog_P_EXT*",pgs13,"+ b_Dnoncog_C_EXT*",pgs21,"+ b_Dnoncog_M_EXT*",pgs22,"+ b_Dnoncog_F_EXT*",pgs23),
+  paste(outcome4, "~ b_Fcog_C_EXT*",pgs11,"+ b_Fcog_P_EXT*",pgs12,"+ b_Fcog_P_EXT*",pgs13,"+ b_Fnoncog_C_EXT*",pgs21,"+ b_Fnoncog_M_EXT*",pgs22,"+ b_Fnoncog_F_EXT*",pgs23),
+  paste(outcome5, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome6, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome7, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome8, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + "))
+)
+
+# Repeat for EXTnc, INTc, INTnc
+
+### --- Time+Parent-invariant models ---
+# EXT Cognitive
+formulas_timeparentinv_EXTc <- c(
+  paste(outcome1, "~ b_cog_C_EXT*",pgs11,"+ b_cog_P_EXT*",pgs12,"+ b_cog_P_EXT*",pgs13,"+ b_Bnoncog_C_EXT*",pgs21,"+ b_Bnoncog_P_EXT*",pgs22,"+ b_Bnoncog_P_EXT*",pgs23),
+  paste(outcome2, "~ b_cog_C_EXT*",pgs11,"+ b_cog_P_EXT*",pgs12,"+ b_cog_P_EXT*",pgs13,"+ b_Cnoncog_C_EXT*",pgs21,"+ b_Cnoncog_P_EXT*",pgs22,"+ b_Cnoncog_P_EXT*",pgs23),
+  paste(outcome3, "~ b_cog_C_EXT*",pgs11,"+ b_cog_P_EXT*",pgs12,"+ b_cog_P_EXT*",pgs13,"+ b_Dnoncog_C_EXT*",pgs21,"+ b_Dnoncog_P_EXT*",pgs22,"+ b_Dnoncog_P_EXT*",pgs23),
+  paste(outcome4, "~ b_cog_C_EXT*",pgs11,"+ b_cog_P_EXT*",pgs12,"+ b_cog_P_EXT*",pgs13,"+ b_Fnoncog_C_EXT*",pgs21,"+ b_Fnoncog_P_EXT*",pgs22,"+ b_Fnoncog_P_EXT*",pgs23),
+  paste(outcome5, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome6, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome7, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome8, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + "))
+)
+
+# Repeat for EXTnc, INTc, INTnc
+
+### --- Component-invariant models ---
+# EXT
+formulas_compinv_EXT <- c(
+  paste(outcome1, "~ b_B_C_EXT*",pgs11,"+ b_B_M_EXT*",pgs12,"+ b_B_F_EXT*",pgs13,"+ b_B_C_EXT*",pgs21,"+ b_B_M_EXT*",pgs22,"+ b_B_F_EXT*",pgs23),
+  paste(outcome2, "~ b_C_C_EXT*",pgs11,"+ b_C_M_EXT*",pgs12,"+ b_C_F_EXT*",pgs13,"+ b_C_C_EXT*",pgs21,"+ b_C_M_EXT*",pgs22,"+ b_C_F_EXT*",pgs23),
+  paste(outcome3, "~ b_D_C_EXT*",pgs11,"+ b_D_M_EXT*",pgs12,"+ b_D_F_EXT*",pgs13,"+ b_D_C_EXT*",pgs21,"+ b_D_M_EXT*",pgs22,"+ b_D_F_EXT*",pgs23),
+  paste(outcome4, "~ b_F_C_EXT*",pgs11,"+ b_F_M_EXT*",pgs12,"+ b_F_F_EXT*",pgs13,"+ b_F_C_EXT*",pgs21,"+ b_F_M_EXT*",pgs22,"+ b_F_F_EXT*",pgs23),
+  paste(outcome5, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome6, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome7, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome8, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + "))
+)
+
+# INT
+formulas_compinv_INT <- c(
+  paste(outcome1, "~ b_B_C_INT*",pgs11,"+ b_B_M_INT*",pgs12,"+ b_B_F_INT*",pgs13,"+ b_B_C_INT*",pgs21,"+ b_B_M_INT*",pgs22,"+ b_B_F_INT*",pgs23),
+  paste(outcome2, "~ b_C_C_INT*",pgs11,"+ b_C_M_INT*",pgs12,"+ b_C_F_INT*",pgs13,"+ b_C_C_INT*",pgs21,"+ b_C_M_INT*",pgs22,"+ b_C_F_INT*",pgs23),
+  paste(outcome3, "~ b_D_C_INT*",pgs11,"+ b_D_M_INT*",pgs12,"+ b_D_F_INT*",pgs13,"+ b_D_C_INT*",pgs21,"+ b_D_M_INT*",pgs22,"+ b_D_F_INT*",pgs23),
+  paste(outcome4, "~ b_F_C_INT*",pgs11,"+ b_F_M_INT*",pgs12,"+ b_F_F_INT*",pgs13,"+ b_F_C_INT*",pgs21,"+ b_F_M_INT*",pgs22,"+ b_F_F_INT*",pgs23),
+  paste(outcome5, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome6, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome7, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + ")),
+  paste(outcome8, "~", paste(c(pgs11,pgs12,pgs13,pgs21,pgs22,pgs23),collapse=" + "))
+)
 
 #############################################
 # BUILD & FIT MODELS
 #############################################
-build_and_fit <- function(formulas) fit_model(make_model(formulas))
-fit_base_svy <- build_and_fit(formulas_base)
+model_baseTidy <- make_model(formulas_base); fit_base_svy <- fit_model(model_baseTidy)
 
-# Nulls
-fit_null_svy_EXTc <- build_and_fit(formulas_null_EXTc)
-fit_null_svy_EXTnc <- build_and_fit(formulas_null_EXTnc)
-fit_null_svy_EXTj <- build_and_fit(formulas_null_EXTj)
-fit_null_svy_INTc <- build_and_fit(formulas_null_INTc)
-fit_null_svy_INTnc <- build_and_fit(formulas_null_INTnc)
-fit_null_svy_INTj <- build_and_fit(formulas_null_INTj)
+model_nullTidy_EXTc <- make_model(formulas_null_EXTc); fit_null_svy_EXTc <- fit_model(model_nullTidy_EXTc)
+model_nullTidy_EXTnc <- make_model(formulas_null_EXTnc); fit_null_svy_EXTnc <- fit_model(model_nullTidy_EXTnc)
+model_nullTidy_EXTj <- make_model(formulas_null_EXTj); fit_null_svy_EXTj <- fit_model(model_nullTidy_EXTj)
 
-# Constrained
-fit_timeinv_svy_EXTc <- build_and_fit(formulas_timeinv_EXTc)
-fit_timeinv_svy_EXTnc <- build_and_fit(formulas_timeinv_EXTnc)
-fit_timeinv_svy_INTc <- build_and_fit(formulas_timeinv_INTc)
-fit_timeinv_svy_INTnc <- build_and_fit(formulas_timeinv_INTnc)
-fit_parentinv_svy_EXTc <- build_and_fit(formulas_parentinv_EXTc)
-fit_parentinv_svy_EXTnc <- build_and_fit(formulas_parentinv_EXTnc)
-fit_parentinv_svy_INTc <- build_and_fit(formulas_parentinv_INTc)
-fit_parentinv_svy_INTnc <- build_and_fit(formulas_parentinv_INTnc)
-fit_timeparentinv_svy_EXTc <- build_and_fit(formulas_timeparentinv_EXTc)
-fit_timeparentinv_svy_EXTnc <- build_and_fit(formulas_timeparentinv_EXTnc)
-fit_timeparentinv_svy_INTc <- build_and_fit(formulas_timeparentinv_INTc)
-fit_timeparentinv_svy_INTnc <- build_and_fit(formulas_timeparentinv_INTnc)
-fit_compinv_svy_EXT <- build_and_fit(formulas_compinv_EXT)
-fit_compinv_svy_INT <- build_and_fit(formulas_compinv_INT)
+model_nullTidy_INTc <- make_model(formulas_null_INTc); fit_null_svy_INTc <- fit_model(model_nullTidy_INTc)
+model_nullTidy_INTnc <- make_model(formulas_null_INTnc); fit_null_svy_INTnc <- fit_model(model_nullTidy_INTnc)
+model_nullTidy_INTj <- make_model(formulas_null_INTj); fit_null_svy_INTj <- fit_model(model_nullTidy_INTj)
+
+model_timeinvTidy_EXTc <- make_model(formulas_timeinv_EXTc); fit_timeinv_svy_EXTc <- fit_model(model_timeinvTidy_EXTc)
+model_timeinvTidy_EXTnc <- make_model(formulas_timeinv_EXTnc); fit_timeinv_svy_EXTnc <- fit_model(model_timeinvTidy_EXTnc)
+model_timeinvTidy_INTc <- make_model(formulas_timeinv_INTc); fit_timeinv_svy_INTc <- fit_model(model_timeinvTidy_INTc)
+model_timeinvTidy_INTnc <- make_model(formulas_timeinv_INTnc); fit_timeinv_svy_INTnc <- fit_model(model_timeinvTidy_INTnc)
+
+model_parentinvTidy_EXTc <- make_model(formulas_parentinv_EXTc); fit_parentinv_svy_EXTc <- fit_model(model_parentinvTidy_EXTc)
+model_parentinvTidy_EXTnc <- make_model(formulas_parentinv_EXTnc); fit_parentinv_svy_EXTnc <- fit_model(model_parentinvTidy_EXTnc)
+model_parentinvTidy_INTc <- make_model(formulas_parentinv_INTc); fit_parentinv_svy_INTc <- fit_model(model_parentinvTidy_INTc)
+model_parentinvTidy_INTnc <- make_model(formulas_parentinv_INTnc); fit_parentinv_svy_INTnc <- fit_model(model_parentinvTidy_INTnc)
+
+model_timeparentinvTidy_EXTc <- make_model(formulas_timeparentinv_EXTc); fit_timeparentinv_svy_EXTc <- fit_model(model_timeparentinvTidy_EXTc)
+model_timeparentinvTidy_EXTnc <- make_model(formulas_timeparentinv_EXTnc); fit_timeparentinv_svy_EXTnc <- fit_model(model_timeparentinvTidy_EXTnc)
+model_timeparentinvTidy_INTc <- make_model(formulas_timeparentinv_INTc); fit_timeparentinv_svy_INTc <- fit_model(model_timeparentinvTidy_INTc)
+model_timeparentinvTidy_INTnc <- make_model(formulas_timeparentinv_INTnc); fit_timeparentinv_svy_INTnc <- fit_model(model_timeparentinvTidy_INTnc)
+
+model_compinvTidy_EXT <- make_model(formulas_compinv_EXT); fit_compinv_svy_EXT <- fit_model(model_compinvTidy_EXT)
+model_compinvTidy_INT <- make_model(formulas_compinv_INT); fit_compinv_svy_INT <- fit_model(model_compinvTidy_INT)
 
 #############################################
 # MODEL COMPARISONS
 #############################################
-# Externalising
 Chisq_00a <- extract_chisq(anova(fit_base_svy, fit_null_svy_EXTc), 'CNC_null_EXTc')
 Chisq_01a <- extract_chisq(anova(fit_base_svy, fit_null_svy_EXTnc), 'CNC_null_EXTnc')
 Chisq_02a <- extract_chisq(anova(fit_base_svy, fit_null_svy_EXTj), 'CNC_null_EXTj')
@@ -200,9 +225,8 @@ Chisq_3a <- extract_chisq(anova(fit_parentinv_svy_EXTc, fit_timeparentinv_svy_EX
 Chisq_4a <- extract_chisq(anova(fit_base_svy, fit_timeinv_svy_EXTnc), 'CNC_timeinv_EXTnc')
 Chisq_5a <- extract_chisq(anova(fit_base_svy, fit_parentinv_svy_EXTnc), 'CNC_parentinv_EXTnc')
 Chisq_6a <- extract_chisq(anova(fit_parentinv_svy_EXTnc, fit_timeparentinv_svy_EXTnc), 'CNC_timeparentinv_EXTnc')
-Chisq_7a <- extract_chisq(anova(fit_base_svy, fit_compinv_svy_EXT), 'CNC_compinv_EXTc')
+Chisq_7a <- extract_chisq(anova(fit_base_svy, fit_compinv_svy_EXT), 'CNC_compinv_EXT')
 
-# Internalising
 Chisq_00b <- extract_chisq(anova(fit_base_svy, fit_null_svy_INTc), 'CNC_null_INTc')
 Chisq_01b <- extract_chisq(anova(fit_base_svy, fit_null_svy_INTnc), 'CNC_null_INTnc')
 Chisq_02b <- extract_chisq(anova(fit_base_svy, fit_null_svy_INTj), 'CNC_null_INTj')
@@ -214,11 +238,12 @@ Chisq_5b <- extract_chisq(anova(fit_base_svy, fit_parentinv_svy_INTnc), 'CNC_par
 Chisq_6b <- extract_chisq(anova(fit_parentinv_svy_INTnc, fit_timeparentinv_svy_INTnc), 'CNC_timeparentinv_INTnc')
 Chisq_7b <- extract_chisq(anova(fit_base_svy, fit_compinv_svy_INT), 'CNC_compinv_INT')
 
-# Combine results
-nullcomp <- rbind(Chisq_02a, Chisq_02b)
+nullcomp <- rbind(Chisq_02a,Chisq_02b)
 comparison <- rbind(Chisq_1a,Chisq_2a,Chisq_3a,Chisq_4a,Chisq_5a,Chisq_6a,
                     Chisq_1b,Chisq_2b,Chisq_3b,Chisq_4b,Chisq_5b,Chisq_6b)
 comparison$p_fdr <- p.adjust(comparison$pvalue, method="BH")
-conscomp <- rbind(Chisq_7a, Chisq_7b)
+conscomp <- rbind(Chisq_7a,Chisq_7b)
 
-print(nullcomp); print(comparison); print(conscomp)
+print(nullcomp)
+print(comparison)
+print(conscomp)
